a8cb43a56949ea978a5fdf5b1c55d977cf4b1600
snoopy369 <snoopy369@users.noreply.github.com>
2017-02-14 13:41:22 -0600

    Create SOQuestionsRemap.js

---
+++ SOQuestionsRemap.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         SO Questions Remap
+// @namespace    
+// @version      0.1
+// @description  Remap StackOverflow "Questions" link on the new navbar (new in 2017)
+// @author       http://stackoverflow.com/users/1623007/joe
+// @include      http*://stackoverflow.com/*
+// @grant        none
+// ==/UserScript==
+
+  document.getElementById("nav-questions").onclick = function() {
+    document.getElementById("nav-questions").href="http://stackoverflow.com/questions/tagged/sas-macro%20sas%20enterprise-guide%20sas-ods%20sas-gtl?mode=any";
+    return true;
+  };
