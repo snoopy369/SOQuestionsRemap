7022726fc00d65bf3ce4c35e87888db07b20256f
snoopy369 <snoopy369@users.noreply.github.com>
2017-02-14 16:37:52 -0600

    Documentation!

--- SOQuestionsRemap.js
+++ SOQuestionsRemap.js
@@ -8,7 +8,12 @@
 // @grant        none
 // ==/UserScript==
 
+  //Adding this functionality to the onClick of the Questions nav link itself.
+  //As a result it won't actually show up in the text to say what you're going to, unfortunately.
   document.getElementById("nav-questions").onclick = function() {
+    
+    //Update the .href below to target the page you want Questions to map to
     document.getElementById("nav-questions").href="http://stackoverflow.com/questions/tagged/YourTagsHere";
-    return true;
+
+    return true;  //This lets the link actually work.
   };
